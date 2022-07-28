5eaff10b79584b7da022891bfc3d3ce60e1c3cae
madou936 <madou936@outlook.fr>
2022-07-28 16:00:00 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,7 +23,6 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->redirectToRoute('show_profile');
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
