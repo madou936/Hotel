7da35b7049b854e57be3dc915873439f26c655d3
madou936 <madou936@outlook.fr>
2022-07-28 15:02:55 +0200

    profile + password

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => [
+                'label' => 'Nouveau mot de passe',
+                'attr' => [
+                    'autocomplete' => 'new-password'
+                ],
+            ],
+            'second_options' => [
+                'label' => 'Répétez le mot de passe',
+                'attr' => [
+                    'autocomplete' => 'new-password'
+                ],
+            ],
+            'invalid_message' => 'Les mots de passe ne sont pas identiques',
+            'mapped' => false
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'Changer votre mot de passe',
+            'validate' => false,
+            'attr' => [
+                'class' => 'd-block mx-auto col-3 my-3 btn btn-success'
+            ]
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
